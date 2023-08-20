7b61872620d0512d432ad4bd3662e53959d14de0
Jack17432 <ChrisGraham908@gmail.com>
2023-08-21 00:12:13 +1200

    added track progression calculator

--- Assets/Scripts/Car/CarStats.cs
+++ Assets/Scripts/Car/CarStats.cs
@@ -1,21 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Car {
     public class CarStats {
         // Car data
         public string carName;
+        private Vector3 carStartingLocation;
+        public int currentLapCount;
+        public Vector3 currentPosition;
         public bool isCrashed;
-        
+
         // Track stats
         public int position;
         public float trackProgress; // % the way around the track
-    }
 
-    public class CarConfig {
-        public string carName;
-        
-        public bool hasLidar;
-        public bool hasImu;
+        public void UpdateTrackProgress(List<Vector3> trackCenterLine, Vector3 carCurrentLocation) {
+            currentPosition = carCurrentLocation;
+            if (trackCenterLine == null || trackCenterLine.Count < 2) return;
 
-        public int lidarHz;
-        public int imuHz;
+            var closestDistance = float.MaxValue;
+            var closestIndex = -1;
+            for (var i = 0; i < trackCenterLine.Count; i++) {
+                var distance = Vector3.Distance(carCurrentLocation, trackCenterLine[i]);
+                if (distance < closestDistance) {
+                    closestDistance = distance;
+                    closestIndex = i;
+                }
+            }
+
+            float totalDistance = 0;
+            float distanceToClosestPoint = 0;
+            float distanceFromStartToClosestPoint = 0;
+            var hasPassedStartingPoint = false;
+
+            for (var i = 1; i < trackCenterLine.Count; i++) {
+                var segmentDistance = Vector3.Distance(trackCenterLine[i], trackCenterLine[i - 1]);
+                totalDistance += segmentDistance;
+
+                if (hasPassedStartingPoint) distanceFromStartToClosestPoint += segmentDistance;
+
+                if (i == closestIndex) {
+                    distanceToClosestPoint = distanceFromStartToClosestPoint;
+                    hasPassedStartingPoint = false;
+                }
+
+                if (trackCenterLine[i] == carStartingLocation) hasPassedStartingPoint = true;
+            }
+
+            if (hasPassedStartingPoint)
+                distanceFromStartToClosestPoint +=
+                    Vector3.Distance(trackCenterLine[0], trackCenterLine[^1]);
+
+            var progress = (distanceToClosestPoint + distanceFromStartToClosestPoint) % totalDistance;
+            trackProgress = progress / totalDistance * 100f;
+            
+            // TODO: make it so that it increases the lap count and keeps track of the time of lap
+
+            trackProgress = Mathf.Clamp(trackProgress, 0, 99);
+        }
     }
 }
--- Assets/Scripts/RacingControllers/EnvironmentController.cs
+++ Assets/Scripts/RacingControllers/EnvironmentController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using UnityEngine;
 
 namespace RacingControllers {
-    
     /*  TODO!: Create race Controller functions to reset cars and create them.
      *  Create cars
      *  Create cars configs
@@ -13,31 +12,32 @@ namespace RacingControllers {
      *  Fix issue with spline killing its self when its not selected in the editor
      *  Add sim time multiplier
      */
-    
+
     [RequireComponent(typeof(SplineCreator))]
     public class EnvironmentController : MonoBehaviour {
-        public bool drawTrackDebugLines = false;
+        public bool drawTrackDebugLines;
 
-        public int trackGenerationSeed = 0;
+        public int trackGenerationSeed;
         public float trackThickness = 5f;
         public float trackWallHeight = 10f;
         public Material trackWallMaterial;
         public CarController carPrefab;
-        
-        private List<Vector3> trackPoints;
+        public List<CarController> listOfCars;
         private List<Vector3> leftEdge;
-        private List<Vector3> rightEdge;
+        private GameObject leftEdgeChild;
 
         private Mesh leftEdgeWallMesh;
-        private Mesh rightEdgeWallMesh;
-        private GameObject leftEdgeChild;
+        private List<Vector3> rightEdge;
         private GameObject rightEdgeChild;
+        private Mesh rightEdgeWallMesh;
 
         private SplineCreator splineCreator;
 
+        private List<Vector3> trackPoints;
+
         private void Start() {
             splineCreator = GetComponent<SplineCreator>();
-            
+
             CreateTrack();
         }
 
@@ -52,7 +52,7 @@ namespace RacingControllers {
         [ContextMenu("Create track")]
         public void CreateTrack() {
             splineCreator.Seed = trackGenerationSeed;
-            
+
             GenerateTrackLines();
             CreateAndRenderWallMeshes();
         }
@@ -61,12 +61,13 @@ namespace RacingControllers {
         public void CreateCarAtStartOfTrack() {
             CreateCarAtPercentAroundTrack(0f);
         }
-        
+
         public void CreateCarAtPercentAroundTrack(float percentage) {
-            Vector3 position = GetPositionOnSpline(trackPoints, percentage, out var rotation);
-            Instantiate(carPrefab, position, rotation);
+            var position = GetPositionOnSpline(trackPoints, percentage, out var rotation);
+            var car = Instantiate(carPrefab, position, rotation);
+            listOfCars.Add(car);
         }
-        
+
         private void GenerateTrackLines() {
             splineCreator.GenerateVoronoi();
             splineCreator.GenerateSpline();
@@ -77,8 +78,8 @@ namespace RacingControllers {
             RemoveSelfIntersections(leftEdge);
             RemoveSelfIntersections(rightEdge);
         }
-        private void CreateAndRenderWallMeshes()
-        {
+
+        private void CreateAndRenderWallMeshes() {
             leftEdgeWallMesh = CreateWallMesh(leftEdge);
             leftEdgeWallMesh = MakeDoubleSided(leftEdgeWallMesh);
             rightEdgeWallMesh = CreateWallMesh(rightEdge);
@@ -87,13 +88,13 @@ namespace RacingControllers {
             leftEdgeChild = CreateOrUpdateChildWithMesh("LeftEdgeChild", leftEdgeWallMesh, leftEdgeChild);
             rightEdgeChild = CreateOrUpdateChildWithMesh("RightEdgeChild", rightEdgeWallMesh, rightEdgeChild);
         }
+
         private GameObject CreateOrUpdateChildWithMesh(string childName, Mesh mesh, GameObject existingChild) {
             GameObject child;
-            if (existingChild == null)
-            {
+            if (existingChild == null) {
                 // If there's no existing child, create a new one
                 child = new GameObject(childName);
-                child.transform.SetParent(this.transform);
+                child.transform.SetParent(transform);
                 child.transform.localPosition = Vector3.zero;
                 child.transform.localRotation = Quaternion.identity;
 
@@ -102,41 +103,42 @@ namespace RacingControllers {
                 child.AddComponent<MeshRenderer>();
                 child.AddComponent<MeshCollider>();
             }
-            else
-            {
+            else {
                 child = existingChild;
             }
 
             // Update the MeshFilter with the new mesh
-            MeshFilter meshFilter = child.GetComponent<MeshFilter>();
+            var meshFilter = child.GetComponent<MeshFilter>();
             meshFilter.mesh = mesh;
 
             // Set the material for the MeshRenderer
-            MeshRenderer meshRenderer = child.GetComponent<MeshRenderer>();
+            var meshRenderer = child.GetComponent<MeshRenderer>();
             meshRenderer.material = trackWallMaterial;
 
             // Set the mesh for the MeshCollider
-            MeshCollider meshCollider = child.GetComponent<MeshCollider>();
+            var meshCollider = child.GetComponent<MeshCollider>();
             meshCollider.sharedMesh = mesh;
 
             return child;
         }
+
         private void DrawSpline(List<Vector3> spline, Color colour) {
             var prevPoint = spline[0];
             foreach (var point in spline) {
                 Debug.DrawLine(prevPoint, point, colour);
                 prevPoint = point;
             }
+
             Debug.DrawLine(spline.Last(), spline[0], colour);
         }
+
         private Mesh CreateWallMesh(List<Vector3> points) {
-            Mesh mesh = new Mesh();
+            var mesh = new Mesh();
 
-            List<Vector3> vertices = new List<Vector3>();
-            List<int> triangles = new List<int>();
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>();
 
-            for (int i = 0; i < points.Count; i++)
-            {
+            for (var i = 0; i < points.Count; i++) {
                 // Add bottom vertex
                 vertices.Add(points[i]);
 
@@ -145,12 +147,11 @@ namespace RacingControllers {
             }
 
             // Generate triangles
-            for (int i = 0; i < points.Count; i++)
-            {
-                int bottomLeft = i * 2;
-                int topLeft = i * 2 + 1;
-                int bottomRight = (i + 1) % points.Count * 2; // Use modulo for looping
-                int topRight = (i + 1) % points.Count * 2 + 1; // Use modulo for looping
+            for (var i = 0; i < points.Count; i++) {
+                var bottomLeft = i * 2;
+                var topLeft = i * 2 + 1;
+                var bottomRight = (i + 1) % points.Count * 2; // Use modulo for looping
+                var topRight = (i + 1) % points.Count * 2 + 1; // Use modulo for looping
 
                 // First triangle
                 triangles.Add(bottomLeft);
@@ -169,52 +170,44 @@ namespace RacingControllers {
 
             return mesh;
         }
+
         private Vector3 GetPositionOnSpline(List<Vector3> spline, float percentage, out Quaternion rotation) {
-            if (spline == null || spline.Count < 2)
-            {
+            if (spline == null || spline.Count < 2) {
                 rotation = Quaternion.identity;
                 return Vector3.zero;
             }
 
-            float totalLength = 0f;
-            for (int i = 0; i < spline.Count - 1; i++)
-            {
-                totalLength += Vector3.Distance(spline[i], spline[i + 1]);
-            }
+            var totalLength = 0f;
+            for (var i = 0; i < spline.Count - 1; i++) totalLength += Vector3.Distance(spline[i], spline[i + 1]);
 
-            float targetLength = totalLength * percentage;
-            float accumulatedLength = 0f;
+            var targetLength = totalLength * percentage;
+            var accumulatedLength = 0f;
 
-            for (int i = 0; i < spline.Count - 1; i++)
-            {
-                float segmentLength = Vector3.Distance(spline[i], spline[i + 1]);
-                if (accumulatedLength + segmentLength >= targetLength)
-                {
-                    float segmentPercentage = (targetLength - accumulatedLength) / segmentLength;
-                    Vector3 direction = (spline[i + 1] - spline[i]).normalized;
+            for (var i = 0; i < spline.Count - 1; i++) {
+                var segmentLength = Vector3.Distance(spline[i], spline[i + 1]);
+                if (accumulatedLength + segmentLength >= targetLength) {
+                    var segmentPercentage = (targetLength - accumulatedLength) / segmentLength;
+                    var direction = (spline[i + 1] - spline[i]).normalized;
                     rotation = Quaternion.LookRotation(direction);
                     return Vector3.Lerp(spline[i], spline[i + 1], segmentPercentage);
                 }
+
                 accumulatedLength += segmentLength;
             }
 
-            Vector3 endDirection = (spline[spline.Count - 1] - spline[spline.Count - 2]).normalized;
+            var endDirection = (spline[spline.Count - 1] - spline[spline.Count - 2]).normalized;
             rotation = Quaternion.LookRotation(endDirection);
             return spline[spline.Count - 1];
         }
-        private static Vector3 GetCenterPoint(List<Vector3> points)
-        {
-            if (points == null || points.Count == 0)
-            {
-                throw new ArgumentException("List of points is null or empty.");
-            }
+
+        private static Vector3 GetCenterPoint(List<Vector3> points) {
+            if (points == null || points.Count == 0) throw new ArgumentException("List of points is null or empty.");
 
             float sumX = 0;
             float sumY = 0;
             float sumZ = 0;
 
-            foreach (Vector3 point in points)
-            {
+            foreach (var point in points) {
                 sumX += point.x;
                 sumY += point.y;
                 sumZ += point.z;
@@ -222,94 +215,78 @@ namespace RacingControllers {
 
             return new Vector3(sumX / points.Count, sumY / points.Count, sumZ / points.Count);
         }
-        private static void TranslatePoints(List<Vector3> points, Vector3 translationAmount)
-        {
-            if (points == null)
-            {
-                throw new ArgumentException("List of points is null.");
-            }
 
-            for (int i = 0; i < points.Count; i++)
-            {
-                points[i] -= translationAmount;
-            }
+        private static void TranslatePoints(List<Vector3> points, Vector3 translationAmount) {
+            if (points == null) throw new ArgumentException("List of points is null.");
+
+            for (var i = 0; i < points.Count; i++) points[i] -= translationAmount;
         }
-        private static void GetEdgeSplines(List<Vector3> spline, float thickness, 
-                                          out List<Vector3> leftEdge, out List<Vector3> rightEdge) {
+
+        private static void GetEdgeSplines(List<Vector3> spline, float thickness,
+                                           out List<Vector3> leftEdge, out List<Vector3> rightEdge) {
             leftEdge = new List<Vector3>();
             rightEdge = new List<Vector3>();
 
-            List<Vector3> perps = new List<Vector3>();
+            var perps = new List<Vector3>();
 
             // Calculate perpendicular vectors for each point
-            for (int i = 0; i < spline.Count; i++)
-            {
+            for (var i = 0; i < spline.Count; i++) {
                 Vector3 tangent;
-        
+
                 // Calculate the tangent vector
                 if (i == 0) // First point
-                {
                     tangent = spline[i + 1] - spline[i];
-                }
                 else if (i == spline.Count - 1) // Last point
-                {
                     tangent = spline[i] - spline[i - 1];
-                }
                 else // Middle points
-                {
                     tangent = spline[i + 1] - spline[i - 1];
-                }
 
                 // Normalize the tangent
                 tangent.Normalize();
 
                 // Compute the perpendicular vector in X-Z (swap x and z and negate the new z)
-                Vector3 perp = new Vector3(tangent.z, 0, -tangent.x);
+                var perp = new Vector3(tangent.z, 0, -tangent.x);
                 perps.Add(perp);
             }
 
             // Generate the left and right splines
-            for (int i = 0; i < spline.Count; i++)
-            {
-                Vector3 perp = perps[i];
+            for (var i = 0; i < spline.Count; i++) {
+                var perp = perps[i];
 
                 // For inner edge smoothing: average adjacent perpendicular vectors
-                if (i > 0 && i < spline.Count - 1)
-                {
-                    perp = (perps[i - 1] + perp + perps[i + 1]) / 3.0f;
-                }
+                if (i > 0 && i < spline.Count - 1) perp = (perps[i - 1] + perp + perps[i + 1]) / 3.0f;
 
                 leftEdge.Add(spline[i] + perp * (thickness / 2));
                 rightEdge.Add(spline[i] - perp * (thickness / 2));
             }
         }
+
         private static bool DoSegmentsIntersect(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4) {
             // Using determinant method to check for intersection
-            float det = (p2.x - p1.x) * (p4.z - p3.z) - (p2.z - p1.z) * (p4.x - p3.x);
-            if (det == 0) return false;  // Parallel segments
+            var det = (p2.x - p1.x) * (p4.z - p3.z) - (p2.z - p1.z) * (p4.x - p3.x);
+            if (det == 0) return false; // Parallel segments
 
-            float lambda = ((p4.z - p3.z) * (p4.x - p1.x) + (p3.x - p4.x) * (p4.z - p1.z)) / det;
-            float gamma = ((p1.z - p2.z) * (p4.x - p1.x) + (p2.x - p1.x) * (p4.z - p1.z)) / det;
+            var lambda = ((p4.z - p3.z) * (p4.x - p1.x) + (p3.x - p4.x) * (p4.z - p1.z)) / det;
+            var gamma = ((p1.z - p2.z) * (p4.x - p1.x) + (p2.x - p1.x) * (p4.z - p1.z)) / det;
 
-            return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+            return 0 < lambda && lambda < 1 && 0 < gamma && gamma < 1;
         }
+
         private static List<Vector3> RemoveSelfIntersections(List<Vector3> spline) {
-            for (int i = 0; i < spline.Count - 1; i++)
-            {
-                for (int j = i + 2; j < spline.Count - 1; j++)
-                {
-                    if (DoSegmentsIntersect(spline[i], spline[i + 1], spline[j], spline[j + 1]))
-                    {
+            for (var i = 0; i < spline.Count - 1; i++) {
+                for (var j = i + 2; j < spline.Count - 1; j++)
+                    if (DoSegmentsIntersect(spline[i], spline[i + 1], spline[j], spline[j + 1])) {
                         // Remove points between i+1 and j
                         spline.RemoveRange(i + 1, j - i);
-                        return RemoveSelfIntersections(spline);  // Recursively clean up further intersections
+                        return RemoveSelfIntersections(spline); // Recursively clean up further intersections
                     }
-                }
             }
+
             return spline;
         }
+
         private Mesh MakeDoubleSided(Mesh mesh) {
-            Mesh doubleSidedMesh = new Mesh();
+            var doubleSidedMesh = new Mesh();
 
             // Get existing vertices and triangles
             var vertices = mesh.vertices;
@@ -324,15 +301,11 @@ namespace RacingControllers {
             triangles.CopyTo(newTriangles, 0);
 
             // Duplicate vertices
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                newVertices[vertices.Length + i] = vertices[i];
-            }
+            for (var i = 0; i < vertices.Length; i++) newVertices[vertices.Length + i] = vertices[i];
 
             // Duplicate and reverse triangles
-            for (int i = 0; i < triangles.Length; i += 3)
-            {
-                int offset = vertices.Length;
+            for (var i = 0; i < triangles.Length; i += 3) {
+                var offset = vertices.Length;
                 newTriangles[triangles.Length + i] = triangles[i + 2] + offset;
                 newTriangles[triangles.Length + i + 1] = triangles[i + 1] + offset;
                 newTriangles[triangles.Length + i + 2] = triangles[i] + offset;
@@ -341,7 +314,7 @@ namespace RacingControllers {
             // Assign the new vertices and triangles to the mesh
             doubleSidedMesh.vertices = newVertices;
             doubleSidedMesh.triangles = newTriangles;
-    
+
             // Recalculate normals for the mesh (important to ensure correct lighting/shading)
             doubleSidedMesh.RecalculateNormals();
 
